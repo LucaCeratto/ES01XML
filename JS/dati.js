7a44532f1cb2053951821f4064607bfc68e78581
Luca Ceratto <l.ceratto.3507@vallauri.edu>
2025-10-02 11:35:34 +0200

    0.1.1

---
+++ JS/dati.js
@@ -0,0 +1,29 @@
+let xmlBiblioteca = `<biblioteca>
+    <libro>
+        <titolo>Il nome Della rosa</titolo>
+        <autore>Umberto Eco</autore>
+        <anno>1980</anno>
+        <genere>Romanzo Sotrico</genere>
+        <editore>Bompiani</editore>
+        <isbn>1451234578451</isbn>
+        <scaffale>A1</scaffale>
+    </libro>
+    <libro>
+        <titolo>Il signore degli anelli</titolo>
+        <autore>JRR Tolkien</autore>
+        <anno>1958</anno>
+        <genere>Romanzo Fantasy</genere>
+        <editore>Bompiani</editore>
+        <isbn>4518756184531</isbn>
+        <scaffale>B6</scaffale>
+    </libro>
+<libro>
+    <titolo>1984</titolo>
+    <autore>George Orwell</autore>
+    <anno>1949</anno>
+    <genere>Distopico</genere>
+    <editore>Secker e Warburg</editore>
+    <isbn>9780451524935</isbn>
+    <scaffale>C3</scaffale>
+</libro>
+</biblioteca>`
